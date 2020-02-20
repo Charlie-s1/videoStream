7deebfcb63b9c3809c0842b3a5a843486afb9f31
charlie-s1 <charlie.d.smith98@gmail.com>
2020-02-20 21:32:29 +0000

    fix upload when moved make links appear in the middle of screen

--- server/server.js
+++ server/server.js
@@ -10,24 +10,28 @@ app.use(up());
 //app.use(express.urlencoded());
 
 app.post("/uploadFiles",async function(req,res){
-  let dir = `pages/files/${req.body.folder}`
+  let dir = path.join(__dirname, `../pages/files/${req.body.folder}/`);
   if (!req.body.lv2){
-    dir = `pages/files/${req.body.folder}/${req.body.lv1}`;
+    dir += req.body.lv1;
   }else{
-    dir = `pages/files/${req.body.folder}/${req.body.lv1}/${req.body.lv2}` ;
+    dir += `${req.body.lv1}/${req.body.lv2}`;
   }
   console.log(dir);
+  organise.newDirs(req.body.folder,req.body.lv1,req.body.lv2)
   if (req.files.video.length > 1){
-    await organise.newDirs(req.body.folder,req.body.lv1,req.body.lv2)
     for (file of req.files.video){
       fs.writeFile(`${dir}/${file.name}`, file.data, (err) => {
         if(err) return console.log(err);
-    
         console.log("up");
       })
     }
     
     res.send(JSON.stringify(req.body.file));
+  }else{
+    fs.writeFile(`${dir}/${req.files.video.name}`, req.files.video.data, (err) => {
+      if(err) return console.log(err);
+      console.log("up");
+    })
   }
   
   
